0afb4c34d684edf0773f77874397558b6fa3ea97
KylleH <158534439+KylleH@users.noreply.github.com>
2024-02-02 00:01:23 -0400

    Add files via upload

---
+++ potential_divider.py
@@ -0,0 +1,9 @@
+def potential_divider(volt, r):
+    voltage_drop = [volt * (i / sum(r)) for i in r]
+    return voltage_drop
+
+voltage_source = 10.0  # Replace with your actual voltage source value
+resistor_values = [100, 200, 300,400,10]  # Replace with your actual resistor values
+
+vdrop = potential_divider(voltage_source, resistor_values)
+print("Voltage drop across resistors:", vdrop)
