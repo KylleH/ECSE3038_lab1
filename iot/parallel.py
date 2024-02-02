0984d409499f920227a200cd6cdae7f48d64929e
KylleH <158534439+KylleH@users.noreply.github.com>
2024-02-02 00:51:24 -0400

    Update parallel.py

--- iot/parallel.py
+++ iot/parallel.py
@@ -2,3 +2,10 @@ def parallel(r):
     parallel_resistance = 1 / sum(1 / i for i in r)
     print( f"{round(parallel_resistance)}"+" ohms")
     
+def potential_divider(volt, resistors):
+    total_resistance = sum(resistors)
+    voltage_drops = [volt * (r / total_resistance) for r in resistors]
+    
+    print("Voltage drop across resistors:")
+    for voltage in voltage_drops:
+        print(f"{voltage:.3f} V")
